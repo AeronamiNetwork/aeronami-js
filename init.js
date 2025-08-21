e104861cbe31696bfb031c74fc108442e1b54ced
Xaildres <109335891+Xaildres@users.noreply.github.com>
2025-08-20 19:45:31 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,1 @@
-SpigotConfig.unknownCommandMessage = colorize(`$(serverPrefix)&7Unknown Command. Type '&c/help&7 for help.')
+SpigotConfig.unknownCommandMessage = colorize(`$(serverPrefix)&7Unknown Command. Type '&c/help&7 for help.`)
